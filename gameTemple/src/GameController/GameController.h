8d0be16cf69c950fe31db1f1f6823c4511889adf
Neighbor Sound <rsp44316@gmail.com>
2018-10-13 22:40:25 +0900

    Update GameController.h

--- gameTemple/src/GameController/GameController.h
+++ gameTemple/src/GameController/GameController.h
@@ -22,20 +22,6 @@ public:
 	enum class GameGroup : ECS::Group
 	{
 		LAYER1,			//テスト用
-		BACK,			//最背面背景
-		BACK_OBJECT,	//背景の鍋や皿などの調理器具
-		BACK_STAFF,		//後ろで働く従業員
-		MASTER,			//おやっさん
-		KITCHENWARE,	//調理台に上に載ったオブジェクト
-		COOKING_AREA,	//調理台や床
-		GIRL,			//女の子
-		RECEIVE_STAFF,	//食材を受け取るスタッフ
-		NOTE,			//音符(食材)
-		EFFECT,			//演出用エフェクト
-		UI,				//時計やスコアゲージ等のUI
-		FADE,			//フェード用
-		PAUSE_UI,		//ポーズ用UI
-		TOP_FADE,		//最前面フェード
 		MAX,			//最大数
 	};
 	GameController();
